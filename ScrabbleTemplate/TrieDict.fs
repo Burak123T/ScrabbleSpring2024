476be9f2a412dd44385191cfdea9172095d33cfb
Burak123T <buoe@itu.dk>
2024-05-03 13:56:00 +0200

    Works a little better, but not done yet

--- ScrabbleTemplate/TrieDict.fs
+++ ScrabbleTemplate/TrieDict.fs
@@ -6,21 +6,28 @@ module internal TrieDict
     }
 
     type Trie() =
-        let root : Node = { child_nodes = Map.empty; is_word = false }
+
+        // Root node which is initially empty
+        let mutable root : Node = { child_nodes = Map.empty; is_word = false }
 
         member Trie.Insert (word: string) =
             let rec insertCharacter charlist (trie_node: Node) =
                 match charlist with
                 | [] -> { trie_node with is_word = true} : Node // Word insertion must have ended here
                 | c::cs ->
-                    let node = { child_nodes = trie_node.child_nodes; is_word = false } : Node
-                    let nextNode = insertCharacter cs node
+                    let nextNode = insertCharacter cs trie_node
                     { trie_node with child_nodes = trie_node.child_nodes |> Map.add c nextNode }
-            insertCharacter (List.ofSeq word) root
+            root <- insertCharacter (List.ofSeq word) root
 
         member Trie.Lookup (word: string) =
             let rec findCharacter charlist (trie_node: Node) =
                 match charlist with
                 | [] -> trie_node.is_word
-            
+                | c::cs -> 
+                    let nextnode = Map.tryFind c trie_node.child_nodes
+                    match nextnode with
+                    | Some (n) -> 
+                        printfn("Found Some n")
+                        findCharacter cs n
+                    | None -> findCharacter cs trie_node    
             findCharacter (List.ofSeq word) root
