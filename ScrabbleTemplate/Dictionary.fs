476831ce70940c9979a9ea7b4a8ecc435afc04b4
Burak123T <buoe@itu.dk>
2024-05-04 15:41:05 +0200

    Updated work on the Trie

--- ScrabbleTemplate/Dictionary.fs
+++ ScrabbleTemplate/Dictionary.fs
@@ -5,11 +5,13 @@ module internal Dictionary
         is_word: bool 
     }
 
-    // Root node which is initially empty
-    let mutable root : Node = { child_nodes = Map.empty; is_word = false }
+    type Dict = Node
 
-    let insert (word: string) =
-        let rec insertCharacter charlist (trie_node: Node) =
+    // Root node which is initially empty
+    let root : Node = { child_nodes = Map.empty; is_word = false }
+
+    let insert (word: string) : Dict =
+        let rec insertCharacter charlist (trie_node: Node) : Dict =
             match charlist with
             | [] -> { trie_node with is_word = true} // Word insertion must have ended here
             | c::cs ->
@@ -21,7 +23,8 @@ module internal Dictionary
                 | None -> 
                     let update_node = insertCharacter cs <| { is_word = false; child_nodes = Map.empty }
                     { trie_node with child_nodes = trie_node.child_nodes |> Map.add c update_node }
-        root <- insertCharacter (List.ofSeq word) root
+        let update_root_node = insertCharacter (List.ofSeq word) root
+        update_root_node
 
     let lookup (word: string) =
         let rec findCharacter charlist (trie_node: Node) =
@@ -35,10 +38,9 @@ module internal Dictionary
                 | None -> false   
         findCharacter (List.ofSeq word) root
 
-    let step c t (trie_node: Node)=
-        match Map.tryFind c trie_node.child_nodes with
-        | Some(n) -> Some(trie_node.is_word, n)
+    let step c (d: Dict) =
+        match Map.tryFind c d.child_nodes with
+        | Some(_) -> Some(d.is_word, d)
         | None -> None
 
-    let empty =
-        root <- { child_nodes = Map.empty; is_word = false }
+    let empty = { child_nodes = Map.empty; is_word = false } : Dict
