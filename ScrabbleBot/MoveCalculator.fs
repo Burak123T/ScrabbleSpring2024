0aa99efb655055407efd6f29a15e90d2b4c86baa
Burak123T <buoe@itu.dk>
2024-05-06 14:47:16 +0200

    simplified backtracking

--- ScrabbleBot/MoveCalculator.fs
+++ ScrabbleBot/MoveCalculator.fs
@@ -1,31 +1,17 @@
 module internal MoveCalculator
 
-    open System 
-    open Parser
     open ScrabbleUtil
     open ScrabbleUtil.Dictionary
-    open ScrabbleUtil.ServerCommunication
-    open MultiSet
-    open System.Text.RegularExpressions
 
-
-    // This function will be used to combine letetrs and find valid words within the dictionary
-    let rec combineLetters (hand: uint32 list) (dict: Dictionary.Dict) (word: string) =
+    let rec nextInput (hand: uint32 list) (dict: Dictionary.Dict) (word: string) =
         match hand with
-        | [] -> 
+        | [] ->
             match lookup word dict with
             | true -> Some word
             | false -> None
-        | w::ws ->
-            let a = combineLetters ws dict (word + string w)
-            let b = combineLetters ws dict (word + string w)
-            if (a = None) && (b = None) then None 
-                else 
-                    if (a.ToString().Length > b.ToString().Length) then a
-                    else b
-    
-    let nextMove (hand: MultiSet.MultiSet<uint32>) (dict: Dictionary.Dict) (word: string) =
-        match isEmpty hand with
-        | true -> None
-        | false -> 
-            (combineLetters (toList hand) dict word)
+        | c::rem ->
+            // Backtracking 
+            let nextResult = nextInput rem dict (word + string c)
+            match nextResult with
+            | Some(w) -> Some(w)
+            | None -> None
